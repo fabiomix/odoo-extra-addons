f01e02f4d44cc44ea9031774efa862da460170ce
fabiomix <fabiomix@users.noreply.github.com>
2021-11-25 21:41:27 +0100

    migrate to v12

--- debug_fields_view/__manifest__.py
+++ debug_fields_view/__manifest__.py
@@ -3,10 +3,10 @@
     "name": "Debug Fields View",
     "summary": "Show fields name in views",
     "description": "Replace fields label with fields name in views (for development purposes).",
-    "version": "10.0.0.1.0",
+    "version": "12.0.0.1.0",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/OCA/OCB/blob/10.0/odoo/addons/base/module/module_data.xml
+    # Check https://github.com/OCA/OCB/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     "category": "Technical Settings",
     "author": "fabiomix",
